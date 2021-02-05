5fd2ad9957ca1040ee9c2972110929edfc2ff508
Abhay Raj Singh <abhayraja4@gmail.com>
2021-02-05 16:11:54 +0530

    Add relevant data

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        child: Column(
-          children: [Text("data"), Text("data")],
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: Container(
+          child: Column(
+            children: [
+              CircleAvatar(
+                  radius: 30,
+                  backgroundImage:
+                      NetworkImage('https://via.placeholder.com/150')),
+              Text("Abhay Raj Singh"),
+              Text("Full stack developer"),
+              Card(
+                margin: EdgeInsets.symmetric(),
+                child: ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text("+91 86zz xxxx yy"),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(),
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text("abhayraja4@gmail.com"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
